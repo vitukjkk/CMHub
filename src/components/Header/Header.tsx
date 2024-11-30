229b5f2acba265b3d2131033ea280736cfcd932a
vitukjkkk <vittorcolom@gmail.com>
2024-11-30 12:15:45 -0300

    terminando header and footer

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,27 @@
 import React from "react";
-import "./Header.module.css";
+import styles from "./Header.module.css";
+
+// ICONS
+
+import Notificacao from '../../assets/icons/notificacao.png';
+import Feed from '../../assets/icons/feed.png';
+import Perfil from '../../assets/icons/perfil.png';
+import Logo_Justa from '../../assets/images/logo_justa.png';
 
 export default function Header() {
     return (
         <header>
-            <h1>Olá Mundo!</h1>
+            <div className="divHeaderLeft">
+                <img src={Notificacao} alt="Ícone notificação" className={styles.icon}/>
+                <img src={Feed} alt="Ícone feed" className={styles.icon}/>
+                <img src={Perfil} alt="Ícone perfil" className={styles.icon}/>
+            </div>
+            <div className="divHeaderCenter">
+                <img id={styles.imgHeaderLogo} src={Logo_Justa} alt="Logo principal" className="logo"/>
+            </div>
+            <div className="divHeaderRight">
+                <input id="inputHeaderSearch" type="text"/>
+            </div>
         </header>
     )
 };
