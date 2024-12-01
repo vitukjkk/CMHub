d3dd0fddfae9c9de57de4190e6f55c0e4a7be16c
vitukjkkk <vittorcolom@gmail.com>
2024-12-01 18:10:05 -0300

    terminando as mensagens por enquanto

--- src/pages/Mensagens/Mensagens.tsx
+++ src/pages/Mensagens/Mensagens.tsx
@@ -1,12 +1,78 @@
 import React from 'react';
 import styles from './Mensagens.module.css';
 
+import Perfil from '../../assets/icons/perfil.png';
+
 export default function Mensagens() {
     return (
-        <div>
-            <p>ester</p>
-            <p>ana</p>
-            <p>maria</p> 
+        <div className={styles.divMensagens}>
+            <div className={styles.divMensagensPrivadas}>
+                <h2 className={styles.h2TitleMensagem}>Privadas</h2>
+                <div className={styles.divMensagensPrivadasOne}>
+                    <img className={styles.imgPerfil} src={Perfil} alt="imagem de perfil"/>
+                    <p>Nome do usuário</p>
+                    <p>Última mensagem</p>
+                    <span>{new Date().getDate()}</span>    
+                </div>
+                <div className={styles.divMensagensPrivadasOne}>
+                    <img className={styles.imgPerfil} src={Perfil} alt="imagem de perfil"/>
+                    <p>Nome do usuário</p>
+                    <p>Última mensagem</p>
+                    <span>{new Date().getDate()}</span>    
+                </div>
+                <div className={styles.divMensagensPrivadasOne}>
+                    <img className={styles.imgPerfil} src={Perfil} alt="imagem de perfil"/>
+                    <p>Nome do usuário</p>
+                    <p>Última mensagem</p>
+                    <span>{new Date().getDate()}</span>    
+                </div>
+                <div className={styles.divMensagensPrivadasOne}>
+                    <img className={styles.imgPerfil} src={Perfil} alt="imagem de perfil"/>
+                    <p>Nome do usuário</p>
+                    <p>Última mensagem</p>
+                    <span>{new Date().getDate()}</span>    
+                </div>
+                <div className={styles.divMensagensPrivadasOne}>
+                    <img className={styles.imgPerfil} src={Perfil} alt="imagem de perfil"/>
+                    <p>Nome do usuário</p>
+                    <p>Última mensagem</p>
+                    <span>{new Date().getDate()}</span>    
+                </div>
+            </div>
+
+            <div className={styles.divMensagensGroups}>
+                <h2 className={styles.h2TitleMensagem}>Grupos</h2>
+                <div className={styles.divMensagensGroupsOne}>
+                    <img className={styles.imgPerfil} src={Perfil} alt="imagem de perfil"/>
+                    <p>Nome do usuário</p>
+                    <p>Última mensagem</p>
+                    <span>{new Date().getDate()}</span>    
+                </div>
+                <div className={styles.divMensagensGroupsOne}>
+                    <img className={styles.imgPerfil} src={Perfil} alt="imagem de perfil"/>
+                    <p>Nome do usuário</p>
+                    <p>Última mensagem</p>
+                    <span>{new Date().getDate()}</span>    
+                </div>
+                <div className={styles.divMensagensGroupsOne}>
+                    <img className={styles.imgPerfil} src={Perfil} alt="imagem de perfil"/>
+                    <p>Nome do usuário</p>
+                    <p>Última mensagem</p>
+                    <span>{new Date().getDate()}</span>    
+                </div>
+                <div className={styles.divMensagensGroupsOne}>
+                    <img className={styles.imgPerfil} src={Perfil} alt="imagem de perfil"/>
+                    <p>Nome do usuário</p>
+                    <p>Última mensagem</p>
+                    <span>{new Date().getDate()}</span>    
+                </div>
+                <div className={styles.divMensagensGroupsOne}>
+                    <img className={styles.imgPerfil} src={Perfil} alt="imagem de perfil"/>
+                    <p>Nome do usuário</p>
+                    <p>Última mensagem</p>
+                    <span>{new Date().getDate()}</span>    
+                </div>
+            </div>
         </div>
     );
 }
